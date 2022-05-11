ed8372d50f4f324d19acd37fafcd810aad344ced
Joshua Raimer <joshraimer@gmail.com>
2022-05-11 15:46:28 -0700

    initial commit for plot generator

---
+++ mixedMessages/main.js
@@ -0,0 +1,20 @@
+// Avengers Movie Plot Generator 
+
+
+//data for the two fighters and the location
+const goodGuys = ['Thor', 'Iron Man', 'Black Widow', 'Captain America', 'Hulk', 'Hawk Eye', 'Dr. Strange', 'Wanda', 'Vision', 'Black Panther'];
+
+const badGuys = ['Thanos', 'Ultron', 'Sandman', 'Green Goblin', 'Mysterio', 'Electro', 'Morbius', 'Dormammu', 'Ronan', 'Ivan Vanko'];
+
+const location = ['New York City', 'Asguard', 'Sakaar', 'Quantum Realm', 'London', 'Tokyo', 'Wakanda', 'Xandar'];
+
+
+//random number for selecting a charactor 
+let randomChar = Math.floor(Math.random() * goodGuys.length);
+
+
+
+ 
+  
+  
+  
